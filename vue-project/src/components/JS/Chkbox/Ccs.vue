18a0760e06377fca019df9bf9aaf0bbde7921334
kim-j-s <kogwing@gmail.com>
2026-01-20 15:54:54 +0900

    input 생성

--- vue-project/src/components/JS/Chkbox/Ccs.vue
+++ vue-project/src/components/JS/Chkbox/Ccs.vue
@@ -1,22 +1,40 @@
 <template>
-  <div class="guide_title">
-    checkbox event
-  </div>
 
-  <Checkbox label="누르면 인풋이 나옵니다." />
+	<div class="cont_wrap">
+		<div class="guide_title">
+			checkbox event
+		</div>
 
-  <p class="guide_text">
-    1. 체크박스 checked에 따라서 하단에 input text 컴포넌트가 노출여부 결정 <br>
-    1.1 노출되는 컴포넌트는 rounter-view? 로 해야하는지 확인
-    2. 노출된 input text에 문구를 입력하면 하단 p 태그에 해당 내용 노출
+		<Checkbox label="누르면 인풋이 나옵니다." />
 
-  </p>
+		<p class="guide_text">
+			1. 체크박스 checked에 따라서 하단에 input text 컴포넌트가 노출여부 결정 <br>
+			1.1 노출되는 컴포넌트는 rounter-view? 로 해야하는지 확인 <br>
+			2. 노출된 input text에 문구를 입력하면 하단 p 태그에 해당 내용 노출
+		</p>
+
+		<Input maxlength="20" wrapperClass="test1" inpClass="inptest1 sdf" />
+		<Input :inpClass="['t1', 't2']"/>
+		<Input placeholder="x" :value="dmpValue1" />
+		<Input type="number" readonly />
+		<Input type="number" maxlength="20" disabled readonly  />
+
+	</div>
 
 
 </template>
 
 <script setup lang="ts">
   import Checkbox from "@/cps/checkbox/checkbox.vue"
+	import Input from "@/cps/input/input.vue"
+
+	import { ref } from "vue"
+
+	// let testData = {
+	// 	dmpValue1: '홍길동',
+	// }
+
+	const dmpValue1 = '홍길동';
 
 
 </script>
---
+++ vue-project/src/cps/input/input.vue
@@ -0,0 +1,80 @@
+<template>
+	<div class="input" :class="wrapperClass">
+		<input class="inp"
+			:class="inpClass"
+			:type="type" 
+			:placeholder="placeholder"
+			v-bind="$attrs"
+			:value="value"
+			:readonly="readonly" 
+			:disabled="disabled"
+		/>
+	</div>
+</template>
+
+
+
+<script setup lang="ts">
+
+	// 지정한곳에서만 속성 상속 받기
+	defineOptions({
+		inheritAttrs: false,
+	});
+	// 지정한곳에서만 속성 상속 받기
+
+	interface Props {
+		// type?: string,
+		wrapperClass?: string | string[] | Record<string, boolean>,
+		inpClass?: string | string[] | Record<string, boolean>,
+		type?: 'text' | 'number' | 'password' | 'email' | 'tel' | 'url' | 'search' | 'date' | 'time' | 'file' | 'month' | 'week',
+		placeholder?: string,
+		value?: string | number,
+		readonly?: boolean,
+		disabled?: boolean,
+	}
+
+	const props = withDefaults(defineProps<Props>(), {
+		type: 'text',
+		placeholder: '기본값',
+		readonly: false,
+		disabled: false,
+	});
+</script>
+
+<!-- <script lang="ts">
+	export default {
+		props: {
+			type: {
+				type: String,
+				default: 'text',
+			},
+			placeholder: {
+				type: String,
+				default: '기본값',
+			}
+		}
+	}
+</script> -->
+
+
+<style lang="scss">
+
+	.input {
+		input {
+			display: block;
+			width: 100%;
+			height: 54px;
+			padding: 4px 16px;
+			font-size: 16px;
+			border-radius: 8px;
+			border: 1px solid #000;
+			&:read-only {
+				background-color: #ddd;
+			}
+			&:disabled {
+				background-color: #333;
+			}
+		}
+	}
+	
+</style>
